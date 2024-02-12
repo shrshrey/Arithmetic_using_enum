697226ae2347b52397346e9b8eaaf4be95b23a06
shreya shreya <shrshrey@sheridancollege.ca>
2024-02-12 13:44:31 -0500

    Arithmetic code using enum following oops concept.

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -12,25 +12,24 @@ import static java.time.Clock.system;
 /** This class calls the method to perform 
  * arithmetic operations based on user input
  * execute the code check the output
- * @author sivagamasrinivasan
+ * @author shreya shreya (991713126)
  * 
  */
-public class Arithmetic 
-{
+public class Arithmetic {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two numbers:");
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        System.out.println("Enter arithmetic operation to Perform (PLUS, MINUS, TIMES, DIVIDE):");
+        Operation operation = Operation.valueOf(in.next().toUpperCase());
+
+        ArithmeticBase r = new ArithmeticBase();
+        double result = r.calculate(m, n, operation);
+
+        System.out.println("Result: " + result);
     }
 }
-
--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -1,39 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
-import java.util.Scanner;
+// Enum to represent arithmetic operations
+enum Operation {
+    PLUS, MINUS, TIMES, DIVIDE
+}
 
-/** This class takes String input plus,minus,divide and times
- * from user and execute the arithmetic operation
- * change the code to use enum instead String and mention the advantage of enum.
- * @author sivagamasrinivasan
+// Interface for arithmetic operations
+interface Calculator {
+    double calculate(double x, double y);
+}
+
+// Implementations of the Calculator interface
+class Addition implements Calculator {
+    public double calculate(double x, double y) {
+        return x + y;
+    }
+}
+
+class Subtraction implements Calculator {
+    public double calculate(double x, double y) {
+        return x - y;
+    }
+}
+
+class Multiplication implements Calculator {
+    public double calculate(double x, double y) {
+        return x * y;
+    }
+}
+
+// Division class should be included in a separate file or within the same file
+class Division implements Calculator {
+    public double calculate(double x, double y) {
+        return x / y;
+    }
+}
+
+/**
+ * This class takes Operation enum input from the user and delegates the arithmetic operation
+ * to the corresponding Calculator implementation.
+ * 
+ * @author shreya shreya (991713126)
  * 
  */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
-                return x + y;
-            case "MINUS":
-                return x - y;
-            case "TIMES":
-                return x * y;
-            case "DIVIDE":
-                return x / y;
+public class ArithmeticBase {
+    double calculate(double x, double y, Operation operation) {
+        Calculator calculator;
+        
+        // Use a factory method or Dependency Injection to instantiate the appropriate calculator
+        switch (operation) {
+            case PLUS:
+                calculator = new Addition();
+                break;
+            case MINUS:
+                calculator = new Subtraction();
+                break;
+            case TIMES:
+                calculator = new Multiplication();
+                break;
+            case DIVIDE:
+                calculator = new Division();
+                break;
             default:
-                throw new AssertionError("Unknown operations " + this);
+                throw new AssertionError("Unknown operation: " + operation);
         }
+        
+        return calculator.calculate(x, y);
     }
-   
 }
